5375e2af0633753d2e403f716c9a2676a8bf7954
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-07 16:30:11 +0100

    feat(components/boxer): add custom props for divider

--- components/boxer/boxer.tsx
+++ components/boxer/boxer.tsx
@@ -3,39 +3,63 @@ import React from 'react';
 import { Box, BoxProps, Divider, Center, HStack } from '@chakra-ui/react';
 
 export interface BoxerProps extends BoxProps {
+  /**
+   * @description Add a Divider before or after the container
+   */
   divider?: React.ReactElement;
-  dividerPosition?: 'top' | 'bottom';
+  /**
+   * @description Color of the default Divider
+   */
+  dividerColor?: BoxProps['color'];
+  /**
+   * @description Height of the default Divider
+   */
+  dividerHeight?: BoxProps['h'];
+  /**
+   * @description Width of the default Divider
+   */
   dividerWidth?: BoxProps['w'];
+  /**
+   * @description Vertical position of the default Divider
+   */
+  dividerPosition?: 'top' | 'bottom';
+  /**
+   * @description Space between Elements
+   */
   spacing?: number | string;
+  /**
+   * @description Defines how the browser distributes space between and around Elements
+   */
   justifyContent?: BoxProps['justifyContent'];
-  localeswitcher?: React.ReactElement;
-  themeswitcher?: React.ReactElement;
+  /**
+   * @description Add custom Elements to the container
+   */
+  children?: React.ReactNode;
 }
 
-export function Boxer(props: BoxerProps) {
-  const {
-    h = '3rem',
-    p = 1,
-    spacing = 4,
-    dividerWidth = '70vw',
-    divider = (
-      <Center>
-        <Divider w={dividerWidth} borderBottom="1px" color="gray.400" orientation="horizontal" />
-      </Center>
-    ),
-    dividerPosition = 'top',
-    children = null,
-    ...rest
-  } = props;
-
+export function Boxer({
+  h = '3rem',
+  p = 1,
+  spacing = 4,
+  justifyContent = 'stretch',
+  dividerColor = 'gray.200',
+  dividerHeight = '1px',
+  dividerWidth = '70vw',
+  divider = (
+    <Center>
+      <Divider w={dividerWidth} borderBottom={dividerHeight} color={dividerColor} orientation="horizontal" />
+    </Center>
+  ),
+  dividerPosition = 'top',
+  children = null,
+  ...rest
+}: BoxerProps) {
   return (
     <>
       {dividerPosition === 'top' && divider}
       <Box minH={h} p={p} {...rest}>
-        <HStack spacing={spacing} justifyContent={props.justifyContent}>
+        <HStack spacing={spacing} justifyContent={justifyContent}>
           {children}
-          {props.localeswitcher && <Box>{props.localeswitcher}</Box>}
-          {props.themeswitcher && <Box>{props.themeswitcher}</Box>}
         </HStack>
       </Box>
       {dividerPosition === 'bottom' && divider}
